bf55da08931e60426c5cc0bfc7de9fa14a293439
Igor Figueiredo <80823011+apenasigordev@users.noreply.github.com>
2021-12-01 18:01:08 +0000

    Somethings updated

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const router = express.Router();
 const indexpages = require("./route/page.js");
 const radiopages = require("./route/radios.js");
+const categorypages = require('./route/category.js');
 app.use(express.static(__dirname+"/public"))
 app.use("/", indexpages);
 app.use("/radio", radiopages)
+app.use("/category", categorypages)
 app.listen(3000);
--- public/player.js
+++ public/player.js
@@ -1,6 +1,2 @@
-var audio = document.getElementById("audio");
-var button = document.getElementById("pbtn");
-function play() {
-  audio.play()
-  button.innerHTML("")
-}
+const audio = document.getElementById("audio");
+const btn = document.getElementById("play")
